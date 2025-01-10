e62ca20d74136fe41e3005f70404c76149b15a1b
JeongTaekCho <cjt3591@gmail.com>
2025-01-10 10:59:00 +0900

    Next15 + React19 보일러 플레이트 셋팅

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,39 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginTypeScript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser'; // default import
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        browser: true,
+        node: true,
+      },
+    },
+    plugins: {
+      react: eslintPluginReact,
+      '@typescript-eslint': eslintPluginTypeScript,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-console': 'warn',
+      'react/prop-types': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {},
+  },
 ];
-
-export default eslintConfig;
